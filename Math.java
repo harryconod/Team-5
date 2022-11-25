3752b81477b985d7fbeace033fee3275b6ee7be8
Abhiram <abhiramguptav@gmail.com>
2022-11-25 09:46:57 +0000

    added mod function

--- Math.java
+++ Math.java
@@ -1,3 +1,11 @@
 public class Math {
-    
+
+    /*
+    Developer : Abhiram
+    Uni ID : 220280464
+    Function : Calculate Modulus of two number
+    */
+    public static int mod(int x, int y){
+        return x%y;
+    }
 }
