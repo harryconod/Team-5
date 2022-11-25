d866d7c8731e6fce84f01662291cca19eadd31ce
Harry Conod <hconod@gmail.com>
2022-11-25 09:43:16 +0000

    Added Max and Min functions

--- Math.java
+++ Math.java
@@ -1,3 +1,28 @@
 public class Math {
-    
+    /*
+    Developer: Harry Conod
+    University ID: 220244134
+    Function: This function takes two integers as input and returns the largest one, or 0 if both are equal
+     */
+    public int Max(int x, int y){
+        if (x>y)
+            return x;
+        else if (x<y)
+            return y;
+        else
+            return 0;
+    }
+    /*
+    Developer: Harry Conod
+    University ID: 220244134
+    Function: This function takes two integers as input and returns the smallest one, or 0 if both are equal
+     */
+    public static int Min(int x, int y){
+        if (x>y)
+            return y;
+        else if (x<y)
+            return x;
+        else
+            return 0;
+    }
 }
