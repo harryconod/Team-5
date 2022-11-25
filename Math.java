98992ed1c655b1c2774e09691ac0f718ac66a479
SurajKumarMR <116290430+SurajKumarMR@users.noreply.github.com>
2022-11-25 10:26:29 +0000

    added power function

--- Math.java
+++ Math.java
@@ -1,3 +1,10 @@
+/*
+   Developer: Suraj Kumar Malayudacherevil Ravi
+    University ID: 220306834
+    Function: Power of two integers
+    */
 public class Math {
-    
+public static int power(int x, int y){
+        return = x^y;
+        }
 }
